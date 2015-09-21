34850484ea96806a4db6dd3b50417d08bf9c19b9
Quinn Hanam <qhanam@ece.ubc.ca>
2015-09-21 16:22:39 -0700

    Updated test.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 var assert = require("assert");
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
+var fs = require("fs");
+//var pd = require("../pattern-detect");
+
+/* Test basic refactorings with no third party libraries and no chaining. */
+describe('Basic', function() {
+
+	/* A contrived example. */
+	describe('simple.js', function() {
+		it('the refactored code does not match the expected', function() {
+
+			/* Get the file names. */
+			var original = "./input/simple.js";
+			var expected = "./input/simple-refactores.js";
+		
+			/* Run the refactoring on the file. */
+
+			/* Check the refactoring is correct. */
+			assert.equal(fs.existsSync(original), true);
+
+		});
+	});
 });
