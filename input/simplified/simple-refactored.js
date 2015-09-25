80c13debe9fcd45c1abd5c95a8a9e7746bc41c2f
Quinn Hanam <qhanam@ece.ubc.ca>
2015-09-25 15:56:33 -0700

    Added example from jaredhanson/passport.

--- input/simplified/simple-refactored.js
+++ input/simplified/simple-refactored.js
@@ -1,7 +1,9 @@
 /* Get the first command line arg. */
 var name = process.argv[2];
+
 /* Asynchronously print the name. */
 function getUserDetails(user, cb){
+
 		if(user) {	
 			process.nextTick(function(){ 
 				/* Call the callback with data. */
@@ -12,9 +14,12 @@ function getUserDetails(user, cb){
 			/* Call the callback with error. */
 			cb("Error: No name given.", null);
 		}
+
 }
+
 /* Get the first command line arg. */
 function getUserDetailsPromised(param0) {
+
     return new Promise(function (resolve, reject) {
         getUserDetails(param0, function(err,data){
                     if(err !== null)
@@ -22,12 +27,18 @@ function getUserDetailsPromised(param0) {
                     resolve(data);
                 });
     });
+
 }
+
 /* Call the async function. */
-getUserDetailsPromised(name).then(function (details) {
-	/* Success handler. */
-	console.log(details);
-}, function (err) {
-    /* Error handler. */
-    console.log(err);
-});
+getUserDetailsPromised(name)
+
+	.then(
+		function (details) {
+			/* Success handler. */
+			console.log(details);
+		}, 
+		function (err) {
+			/* Error handler. */
+			console.log(err);
+		});
