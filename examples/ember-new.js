3d3e0d0e1c9bb7d43017ec837e939a6cf9b8364d
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2015-09-25 15:24:30 -0700

    examples from pull requests

---
+++ examples/ember-new.js
@@ -0,0 +1,62 @@
+App = Ember.Application.create({
+});
+
+App.GoogleFile = Ember.Object.extend({
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+
+  model: function(){
+    var model = App.GoogleFile.create({
+      id: $('meta[name="google-folder-id"]').attr('content');,
+      children: Ember.ArrayProxy.create({content: []})
+    });
+
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      this.loadChildren(model, resolve, reject);
+    }.bind(this));
+  },
+
+  // The afterModel won't fire until the promise is fullfilled.
+  afterModel: function(transition){
+    if(transition.targetName == "application.index"){
+      this.transitionTo('anotherPlace');
+    }
+  },
+
+  loadChildren: function(node, resolve, reject){
+  var token = $('meta[name="google-access-token"]').attr('content');
+    var query = encodeURIComponent('"' + node.get('id') + '" in parents');
+    // Don't resolve the promise when the ajax call returns. We have to process the data and decide if we need to make more calls.
+    $.get('https://www.googleapis.com/drive/v2/files?q=' + query + '&access_token=' + token, function(data){
+      var promises = [];
+      data.items.forEach(function(item){
+        var f = App.GoogleFile.create({
+          name: item.title,
+          id: item.id,
+          icon: item.iconLink,
+          mime: item.mimeType,
+          embed: item.embedLink,
+          edit: item.alternateLink,
+          children: Ember.ArrayProxy.create({content: []})
+        });
+
+        if(item.mimeType === "application/vnd.google-apps.folder"){
+          // We need to make more ajax calls. Create a new promise which can be resposible for
+          // resolving existing promises once it is fullfilled.
+          var promise = new Ember.RSVP.Promise(function(resolve, reject){
+            this.loadChildren(f, resolve, reject);
+          }.bind(this));
+          promises.push(promise);
+        }
+        node.get('children').pushObject(f);
+      }.bind(this));
+
+      Promise.all(promises).then(function(){
+        resolve(node);
+      });
+
+    }.bind(this));
+  }
+
+});
---
+++ examples/ember-old.js
@@ -0,0 +1,56 @@
+App.GoogleFile = Ember.Object.extend({
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+
+  model: function(){
+    var model = App.GoogleFile.create({
+      id: $('meta[name="google-folder-id"]').attr('content');
+      children: Ember.ArrayProxy.create({content: []})
+    });
+    this.loadChildren(model.get('children'));
+    return model;
+  },
+
+  afterModel: function(transition){
+    if(transition.targetName == "application.index"){
+      this.wait(model.get('children'), function(){
+        this.transitionTo('anotherPlace');
+      });
+    }
+  },
+
+  // Waiting for content to load using a timer.
+  wait: function(children, callback){
+    if(Ember.isEmpty(children)){
+      Ember.run.later(this, function () {
+        this.wait(children);
+      }, 10);
+    } else {
+      callback(children);
+    }
+  },
+
+  loadChildren: function(node){
+    var token = $('meta[name="google-access-token"]').attr('content');
+    var query = encodeURIComponent('"' + node.get('id') + '" in parents');
+    $.get('https://www.googleapis.com/drive/v2/files?q=' + query + '&access_token=' + token, function(data){
+      data.items.forEach(function(item){
+        var f = App.GoogleFile.create({
+          name: item.title,
+          id: item.id,
+          icon: item.iconLink,
+          mime: item.mimeType,
+          embed: item.embedLink,
+          edit: item.alternateLink,
+          children: Ember.ArrayProxy.create({content: []})
+        });
+        if(item.mimeType === "application/vnd.google-apps.folder"){
+          this.loadChildren(f);
+        }
+        node.get('children').pushObject(f);
+      }.bind(this));
+    }.bind(this));
+  }
+
+});
